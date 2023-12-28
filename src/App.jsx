51ad20e5dcd1e3b73af7101c3153ee9e665f03bf
imtiazsakib15 <sakib.imtiaz15@gmail.com>
2023-12-29 00:55:18 +0600

    Color change functionality added

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,41 @@
 import { useState } from "react";
 
 function App() {
-  const [fontSize, setFontSize] = useState(12);
-  console.log(fontSize);
+  const [fontSize, setFontSize] = useState(16);
+  const [color, setColor] = useState("#000");
+  console.log(color);
   return (
     <>
       <div className="border-2 border-cyan-500 flex gap-5 rounded mx-4 sm:mx-8 md:mx-12 lg:mx-16 my-12 sm:my-16 lg:my-20 p-5">
         <textarea
-          className={`border border-gray-700 w-5/6 text-[${fontSize}px]`}
+          style={{ fontSize: `${fontSize}px`, color:`${color}` }}
+          className="border border-gray-700 rounded w-5/6 min-h-48 md:min-h-96 p-5"
           name="message"
           id="message"
-          rows="10"
           placeholder="New Text ..."
         ></textarea>
-        <div className="w-1/6 border border-gray-700 p-4">
-          <div>
-            <input
-              type="number"
-              onChange={(e) => setFontSize(e.target.value)}
-              defaultValue={fontSize}
-              min={1}
-            />
+        <div className="w-1/6 border border-gray-700 p-4 rounded">
+          <div className="flex gap-8">
+            <div>
+              <label htmlFor="font_size">Font Size</label>
+              <input
+                className="w-12 border"
+                id="font_size"
+                type="number"
+                onChange={(e) => setFontSize(e.target.value)}
+                defaultValue={fontSize}
+                min={1}
+              />
+            </div>
+            <div>
+              <label htmlFor="color">Color</label>
+              <input
+                type="color"
+                id="color"
+                onChange={(e) => setColor(e.target.value)}
+                defaultValue={color}
+              />
+            </div>
           </div>
         </div>
       </div>
